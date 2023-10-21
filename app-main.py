866a3ee62e878260dd7f6075ab825ecef636d7f9
Sarah <87375273+IT21009686@users.noreply.github.com>
2023-10-21 13:26:04 +0530

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -13,7 +13,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
 
 
 # Load your stroke dataset
